cb1cb922746574db128eb8621258ce074c8b7314
Dipen Lama <lamadipen@yahoo.com>
2015-07-06 17:30:22 +0545

    Added jdbc template features

---
+++ src/main/java/com/radius/utility/XmlQueryMap.java
@@ -0,0 +1,18 @@
+package com.radius.utility;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class XmlQueryMap {
+	
+	private Map<String, String> xmlQuery = new HashMap<String, String>();
+
+	public Map<String, String> getXmlQuery() {
+		return xmlQuery;
+	}
+
+	public void setXmlQuery(Map<String, String> xmlQuery) {
+		this.xmlQuery = xmlQuery;
+	}
+
+}
